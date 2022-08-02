3cd54cef93c897c5581542f04e878d1b0a026e34
James. H. Stephenson <james_h_stephenson@outlook.com>
2022-08-02 15:15:20 +0100

    Setup Initial System for everything else, just waiting on OpenSeas API Key to finish.

--- System.py
+++ System.py
@@ -1,1 +1,32 @@
-print("hi")
+#OpenSeas - AssetVerification
+
+#SystemImports
+from asyncio import sleep
+import requests
+import json
+import time
+
+#AssetTable
+Asset_Names = ["Orchid", "Planeterium", "Orenda", "Professor Eve", "Grimoire", "PAL"]
+Asset_Contract_Address = ["0xa342f5d851e866e18ff98f351f2c6637f4478db5"]
+Asset_Contract_IDs = ["82344359815298119800542564839349904627894644716845183454722829717648818845702", "82344359815298119800542564839349904627894644716845183454722829717648818845701", "82344359815298119800542564839349904627894644716845183454722829717648818845700", "82344359815298119800542564839349904627894644716845183454722829717648818845698", "82344359815298119800542564839349904627894644716845183454722829717648818845699", "82344359815298119800542564839349904627894644716845183454722829717648818845697", "82344359815298119800542564839349904627894644716845183454722829717648818845696"]
+
+#PreSetRequest
+OpenSeas_StartURL = "https://api.opensea.io/api/v1/asset/"
+OpenSeas_EndURL = "/owners?limit=20&order_by=created_date&order_direction=desc"
+
+#UserInput
+User_Wallet = ""
+
+#Functions
+def FindAssetOwners():
+    #FunctionVariables
+    System_Counter = 0
+    JSON_Key = ""
+    
+
+def GetUserWallet():
+    User_Wallet = input(str("Input Your Public Wallet Address: "))
+
+GetUserWallet()
+FindAssetOwners()
